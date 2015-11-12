d48dc26ecb80657059e1d7084538703eda838989
Tobias Pape <tobias@netshed.de>
2015-11-12 11:29:32 +0100

    Initial Commit

---
+++ CrayonsInfo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrayonsInfo
+//
+//  Created by Tobias Pape on 12.11.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
