4f71c589143cacd57799e65dca3417b148fb1360
Tobias Pape <tobias@netshed.de>
2015-11-12 14:05:29 +0100

    All rgb for Crayons, writes out crayons to file

--- CrayonsInfo/main.m
+++ CrayonsInfo/main.m
@@ -7,11 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+@import AppKit;
+
+#define bcomponent(cgfloatcomponent) ((int)round((cgfloatcomponent) * 255.0))
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSColorList *colorList = [NSColorList colorListNamed: @"Crayons"];
+        for (NSString *key in [colorList allKeys]) {
+            NSColor *color = [colorList colorWithKey:key];
+            printf("%-10s\t", [key UTF8String]);
+            printf("r: %3d g: %3d b: %3d\n",
+                  bcomponent(color.redComponent),
+                  bcomponent(color.greenComponent),
+                  bcomponent(color.blueComponent));
+        }
+        if (!([[NSFileManager defaultManager] createDirectoryAtPath:@"OldCrayons.clr"
+                                        withIntermediateDirectories:NO
+                                                         attributes:nil
+                                                              error:nil])){
+            return 1;
+        }
+        [colorList writeToFile: @"OldCrayons.clr/OldCrayons.clr"];
     }
     return 0;
 }
